4fa812d504658cb3cf9df8b346722bb703fb68eb
Sreenath <sreenathsakthivel@gmail.com>
2022-11-15 02:15:12 +0530

    Added Sprint - 2

--- Project Development Phase/Sprint 2/app.py
+++ Project Development Phase/Sprint 2/app.py
@@ -24,8 +24,9 @@ try:
 except:
     print("Connection Failed")
     print(ibm.conn_error())
+    
 
-
+ 
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -40,10 +41,21 @@ def forgot():
 
 @app.route('/signup', methods=['POST','GET'])
 def signup():
-     #if request.method == 'POST':
-      #   name = request.form['name']
-       #  email = request.form['email']
-        # phone = request.form['phone']
+    if request.method == 'POST':
+        # conn = connection()
+         try:
+            sql = "INSERT INTO users VALUES('{}','{}','{}','{}')".format(request.form["name"],request.form["email"],request.form["phone"],request.form["password"])
+            ibm_db.exec_immediate(conn,sql)
+            #flash("successfully Registered !")
+            return render_template('login.html')
+         except:
+            #flash("Account already exists! ")
+            return render_template('signup.html')
+    else:
+            return render_template('signup.html')
+        # name = request.form['name']
+         #email = request.form['email']
+         #phone = request.form['phone']
          #password = request.form['password']
         
          #sql ="INSERT INTO users VALUES (?,?,?,?)"
@@ -53,15 +65,39 @@ def signup():
          #ibm_db.bind_param(stmt, 3, phone)
          #ibm_db.bind_param(stmt, 4, password)
          #ibm_db.execute(stmt)
-     return render_template('signup.html')
+   # return render_template('signup.html')
 
 @app.route('/login', methods=['POST','GET'])
 def login():
-    #if request.method == 'POST':
+     if request.method == 'POST':
+       # conn =connection()
+        email = request.form["email"]
+        password = request.form["password"]
+        sql = "SELECT COUNT(*) FROM users WHERE EMAIL=? AND PASSWORD=?"
+        stmt = ibm_db.prepare(conn,sql)
+        ibm_db.bind_param(stmt, 1, email)
+        ibm_db.bind_param(stmt, 2, password)
+        ibm_db.execute(stmt)
+        res = ibm_db.fetch_assoc(stmt)
+        if res['1'] == 1:
+            session['loggedin'] = True
+            session['email'] = email
+            return render_template('userpage.html')
+        else:
+            #flash("email/ Password isincorrect! ")
+            return render_template('login.html')
+     else:
+            return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('username', None)
+    return redirect(url_for('login'))
+
         #email = request.form['email']
         #password = request.form['password']
-        
-        
+  
 
         #sql = "SELECT * FROM users WHERE email=%s AND password=%s"
         #stmt = ibm_db.prepare(conn, sql)
@@ -69,9 +105,8 @@ def login():
         #ibm_db.bind_param(stmt,2,password)
        # user = ibm_db.execute(stmt).fetchone()
         
-    return render_template('login.html' ,msg="success")
+  #    return render_template('login.html' ,msg="success")
 
 if __name__=='__main__':
-        app.run(debug=True)
-
-       
+    app.config['SESSION_TYPE']= 'filesystem'
+    app.run(debug=True)
